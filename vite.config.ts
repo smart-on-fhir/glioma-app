01dc597b3bae3660f8047c6dbadb567d19ece10d
Vladimir Ignatov <vlad.ignatov@gmail.com>
2026-02-03 15:27:37 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 // Read REPO_BASE or VITE_BASE_URL environment variables at build time; default to '/'
-const repoBase = process.env.REPO_BASE ?? process.env.VITE_BASE_URL ?? '/'
+const repoBase = process.env.VITE_BASE_URL ?? '/'
 
 export default defineConfig({
   base: repoBase,
