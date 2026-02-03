4c30065d1aaffb9dad570c0c2678dd476e68a441
Vladimir Ignatov <vlad.ignatov@gmail.com>
2026-02-03 15:36:31 -0500

    Fixed patient link

--- src/components/PatientListItem.tsx
+++ src/components/PatientListItem.tsx
@@ -5,7 +5,7 @@ import { formatDate } from "../utils";
 export default function PatientListItem({ patient }: { patient: Patient }) {
     return (
         <div className="my-5">
-            <h4><Link to={`./${patient.mrn}`}>{patient.name}</Link></h4>
+            <h4><Link to={`/${patient.mrn}`}>{patient.name}</Link></h4>
             <p className="text-success">
                 <span className="text-muted">Gender: </span>{patient.gender}
                 <span className="text-muted">&nbsp;&nbsp;&nbsp;DOB: </span>{formatDate(patient.dob)}
